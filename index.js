bd0c1196e5bcb55a82f58636c231e9ce25947e63
Sr-Kaio <caioprobr6@gmail.com>
2024-02-06 20:24:08 -0300

    'untitled'

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const lenguageButton = document.getElementById("LanguageButton")
+const MlenguageButton = document.getElementById("MobileLanguageButton")
 const lenguagePopup = document.getElementById("LanguagePopup")
 
 lenguageButton.addEventListener("click", function(){
+    lenguagePopup.style.visibility = "visible";
+})
+
+MobileLanguageButton.addEventListener("click", function(){
     lenguagePopup.style.visibility = "visible";
 })
