8119eb0e1aabe5db67464bc6cce80164e0206577
Sr-Kaio <caioprobr6@gmail.com>
2024-02-06 19:29:01 -0300

    'untitled'

---
+++ index.js
@@ -0,0 +1,6 @@
+const lenguageButton = document.getElementById("LanguageButton")
+const lenguagePopup = document.getElementById("LanguagePopup")
+
+lenguageButton.addEventListener("click", function(){
+    lenguagePopup.style.visibility = "visible";
+})
