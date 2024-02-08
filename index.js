dea34a0dcda71e7088e30782d60ca56503ac1213
Sr-Kaio <caioprobr6@gmail.com>
2024-02-07 21:10:58 -0300

    'untitled'

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function LenguagePopup() {
     windows.style.transform = "translateY(0px)";
     lenguagePopup.style.visibility = "visible";
 }
+
+function x() {
+    window.location.href = "https://twitter.com/SrKaio05497576";
+}
+
+function yt() {
+    window.location.href = "https://www.youtube.com/channel/UCBpRtXk8uxsg1QOgn1ptQdQ";
+}
+
+function instagram() {
+    window.location.href = "https://www.instagram.com/ka_io._/";
+}
